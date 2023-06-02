e3fa80603b1a57975746fa69228d6212f9e7039e
Daveedick <davvo77@gmail.com>
2023-06-02 22:43:48 +0530

    Add primevue and unocss

--- cdc_draft/nuxt.config.ts
+++ cdc_draft/nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  imports: {
-      autoImport: false
-    }
+  css: [
+      "primevue/resources/themes/lara-light-blue/theme.css",
+      "primevue/resources/primevue.css",
+      "primeicons/primeicons.css"
+  ],
+  build: {
+    transpile: ["primevue"]
+  },
+  modules: [
+    '@unocss/nuxt',
+  ],
+
 })
