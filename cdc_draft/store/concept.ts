840538e2090e20e0285c114c4433876d9d1c1fd0
Daveedick <davvo77@gmail.com>
2023-07-24 19:42:30 +0530

    in progress

--- cdc_draft/store/concept.ts
+++ cdc_draft/store/concept.ts
@@ -11,25 +11,29 @@ export const useConceptStore = defineStore('conceptStore', () => {
       {
         title: 'Выберите основу',
         description: 'Клиент выбирает понравившееся изделие из вашего каталога моделей и переходит в примерочную',
-				imgURL: 'images/model-sample.png',
+				// imgURL: 'images/model-sample.png',
+				imgURL: 'images/sample1.jpg',
 				imgAlt: 'Sample 1'
       },
       {
         title: 'Меняйте детали и цвета',
         description: 'В конструкторе клиент изменяет доступные детали изделия и их цвета в режиме онлайн',
-				imgURL: 'images/model-sample.png',
+				// imgURL: 'images/model-sample.png',
+				imgURL: 'images/sample2.jpg',
 				imgAlt: 'Sample 2'
       },
       {
         title: 'Получите визуализацию в 3D',
         description: 'В итоге клиент получает полноценную визуализацию своего готового дизайна в 3D',
-				imgURL: 'images/model-sample.png',
+				// imgURL: 'images/model-sample.png',
+				imgURL: 'images/sample3.jpg',
 				imgAlt: 'Sample 3'
       },
       {
         title: 'Оформляйте заказ онлайн',
         description: 'Для оформления заказа клиент выбирает рост, количество размеров и единиц в заказе, а далее получает итоговую стоимость партии',
-				imgURL: 'images/model-sample.png',
+				// imgURL: 'images/model-sample.png',
+				imgURL: 'images/sample4.jpg',
 				imgAlt: 'Sample 4'
       },
 ],
